6962827c065b250f546a69948014acc33e159160
DAMICHOI <choidami93@gmail.com>
2021-10-12 23:08:43 +0900

    feat($Cars) : 레이스의 승자 구하는 로직 추가
    - 테스트 코드 추가(CarsTest.java)
    - 레이스의 승자 구하는 로직 추가

--- src/main/java/racinggame/Cars.java
+++ src/main/java/racinggame/Cars.java
@@ -1,19 +1,22 @@
 package racinggame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Cars {
+	private static final String STRING_SEPERATOR = ",";
 	private final List<Car> cars;
 
-	public Cars(List<String> cars) {
+	public Cars(String carNames) {
+		List<String> cars = Arrays.asList(carNames.split(STRING_SEPERATOR));
 		this.cars = initCar(cars);
 	}
 
 	/**
-	 * 경주용 차 이름 입력
+	 * 경주용 자동차들의 이름을 입력하여 초기화한다.
 	 * @param cars 자동차 이름 리스트
 	 * @return List<Car>
 	 */
@@ -25,6 +28,10 @@ public class Cars {
 		return carList;
 	}
 
+	/**
+	 * 자동차들의 상태를 Map으로 출력한다.
+	 * @return Map<String, Integer>
+	 */
 	public Map<String, Integer> raceStatusMap() {
 		Map<String, Integer> carMap = new HashMap<>();
 		for (Car car : cars) {
@@ -33,7 +40,40 @@ public class Cars {
 		return carMap;
 	}
 
+	/**
+	 * 자동차들이 주행한다.
+	 */
 	public void race() {
 		cars.forEach(Car::race);
+	}
+
+	public Car get(int i) {
+		return cars.get(i);
+	}
+
+	/**
+	 * 레이스의 승자를 구한다.
+	 * @return String
+	 */
+	public String raceWinner() {
+		String winner = "";
+		int farthestPosition = farthestPosition();
+		for (Car car : cars) {
+			winner += (car.getCarPosition() == farthestPosition) ?
+				(winner.length() == 0) ? car.getCarName() : "," + car.getCarName() : "";
+		}
+		return winner;
+	}
+
+	/**
+	 * 자동차들 중 가장 먼 위치 값을 구한다.
+	 * @return int
+	 */
+	private int farthestPosition() {
+		int farthestPosition = 0;
+		for (Car car : cars) {
+			farthestPosition = Math.max(car.getCarPosition(), farthestPosition);
+		}
+		return farthestPosition;
 	}
 }
--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,34 +1,59 @@
 package racinggame;
 
-import java.util.Arrays;
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarsTest {
 	Cars cars;
 
 	@BeforeEach
 	void setUp() {
-		cars = new Cars(Arrays.asList("pobi","crong","honux"));
+		cars = new Cars("pobi,crong,honux");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"pobi", "crong", "honux"})
+	@DisplayName("자동차들의 이름이 pobi, crong, honux 로 초기화된다.")
+	void race_init_cars_name(String carName) {
+		Map<String, Integer> map = cars.raceStatusMap();
+		assertThat(map.containsKey(carName)).isTrue();
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {"pobi", "crong", "honux"})
+	@DisplayName("모든 자동차들의 초기 위치는 0이다.")
+	void race_init_cars_position(String carName) {
+		Map<String, Integer> map = cars.raceStatusMap();
+		assertThat(map.get(carName)).isEqualTo(0);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0:GO:pobi:1", "1:STOP:crong:0", "2:GO:honux:1"}, delimiter = ':')
+	@DisplayName("자동차들의 이동 상태에 따라 위치를 확인한다. pobi, honux은 GO 상태이므로 위치가 1, crong은 STOP 상태이므로 위치가 0이다.")
+	void race_cars_position_by_move_status(int index, String moveStatus, String carName, int position) {
+		cars.get(index).move(getMoveStatus(moveStatus));
+		Map<String, Integer> map = cars.raceStatusMap();
+		assertThat(map.get(carName)).isEqualTo(position);
+	}
+
+	private MoveStatus getMoveStatus(String moveStatus) {
+		return moveStatus.equals("GO") ? MoveStatus.GO : MoveStatus.STOP;
 	}
 
 	@Test
-	@DisplayName("모든 자동차들의 초기 위치는 0이다.")
-	void race_init() {
-		Map<String, Integer> map = cars.raceStatusMap();
-
-		assertThat(map.containsKey("pobi")).isTrue();
-		assertThat(map.containsKey("crong")).isTrue();
-		assertThat(map.containsKey("honux")).isTrue();
-		assertThat(map.get("pobi")).isEqualTo(0);
-		assertThat(map.get("crong")).isEqualTo(0);
-		assertThat(map.get("honux")).isEqualTo(0);
+	@DisplayName("레이싱 게임의 승자는 pobi, honux이다.")
+	void race_winner() {
+		cars.get(0).move(MoveStatus.GO);
+		cars.get(1).move(MoveStatus.STOP);
+		cars.get(2).move(MoveStatus.GO);
+		assertThat(cars.raceWinner()).isEqualTo("pobi,honux");
 	}
 }
